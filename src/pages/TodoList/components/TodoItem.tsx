785f2c8a544451d34d3a124358106b2d0c0a09d0
joe <joe.chang1014@gmail.com>
2022-04-08 15:27:35 +0800

    feat add and delete

--- src/pages/TodoList/components/TodoItem.tsx
+++ src/pages/TodoList/components/TodoItem.tsx
@@ -1,5 +1,27 @@
-const TodoItem: React.FC = () => {
-  return <></>;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark, faPen } from "@fortawesome/free-solid-svg-icons";
+
+type todoItem = {
+  id: number;
+  title: string;
+  isDone: boolean;
+  deleteTodo: (id: number) => void;
+};
+
+const TodoItem: React.FC<todoItem> = ({ id, title, isDone, deleteTodo }) => {
+  return (
+    <li>
+      <label
+        htmlFor="
+        "
+      >
+        <input type="checkbox" checked={isDone} />
+        <input value={title} type="text" readOnly />
+        <FontAwesomeIcon icon={faXmark} onClick={() => deleteTodo(id)} />
+        <FontAwesomeIcon icon={faPen} />
+      </label>
+    </li>
+  );
 };
 
 export default TodoItem;
--- src/pages/TodoList/index.tsx
+++ src/pages/TodoList/index.tsx
@@ -3,18 +3,60 @@ import Header from "../../components/Header";
 import TabList from "./components/TabList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import TodoItem from "./components/TodoItem";
 const TodoList: React.FC = () => {
-  const [tab, setTab] = useState("all");
-  const [list, setList] = useState([]);
+  type TodoType = {
+    id: number;
+    title: string;
+    isDone: boolean;
+  };
+  const [tab, setTab] = useState<string>("all");
+  const [list, setList] = useState<TodoType[]>([
+    { id: 1649401936476, title: "吃早餐", isDone: false },
+    { id: 1649401936477, title: "慢跑", isDone: false },
+    { id: 1649401936478, title: "跳舞", isDone: true },
+  ]);
   const dataList = [];
+  const addTodo = () => {
+    const addInput = document.getElementById("addContent") as HTMLInputElement;
+    const id = Date.now();
+    setList((prevState) => {
+      return [
+        ...prevState,
+        {
+          id,
+          title: addInput.value,
+          isDone: false,
+        },
+      ];
+    });
+  };
+  const deleteTodo = (id: number) => {
+    const listTemp = [...list];
+    setList(listTemp.filter((item) => item.id !== id));
+  };
   return (
     <>
       <Header />
       <TabList setTab={setTab} />
       <div className="w-full rounded-sm md:w-4/5 md:max-w-[500px]">
-        <input maxLength={50} type="text" name="" id="" />
-        <FontAwesomeIcon icon={faPlus} />
-        <ul></ul>
+        <input maxLength={50} type="text" name="" id="addContent" />
+        <FontAwesomeIcon icon={faPlus} onClick={addTodo} />
+        <ul>
+          {list
+            .filter((item: TodoType) => {
+              if (tab === "pending") {
+                return !item.isDone;
+              } else if (tab === "completed") {
+                return item.isDone;
+              } else {
+                return item;
+              }
+            })
+            .map((item) => (
+              <TodoItem {...item} deleteTodo={deleteTodo} />
+            ))}
+        </ul>
       </div>
     </>
   );
